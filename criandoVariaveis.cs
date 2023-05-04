930af4f33275663f906468d6429b6e2a483c5082
Ts_217 <67065618+tss217@users.noreply.github.com>
2023-05-04 11:14:13 -0300

    Add files via upload

---
+++ criandoVariaveis.cs
@@ -0,0 +1,31 @@
+
+using System;
+
+class CriandoVariaveis
+{
+    static void Main(string[]args)
+    {
+        int idade;
+        idade = 27;
+        Console.WriteLine(idade);
+        Console.WriteLine();
+
+        double salario;
+        salario = 3000.15;
+        Console.WriteLine(salario);
+        Console.WriteLine();
+
+        double idadeDois;
+        idadeDois = 7/5;
+        Console.WriteLine(idadeDois);
+        idadeDois = 7.0 / 5;
+        Console.WriteLine(idadeDois);
+        
+
+
+        Console.WriteLine("tecle enter para fechar ...");
+        Console.ReadLine();
+
+
+    }
+}
