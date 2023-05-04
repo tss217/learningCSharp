59be7712f13f166ac967f24e1c9a66c7e9319f79
Ts_217 <67065618+tss217@users.noreply.github.com>
2023-05-04 10:24:41 -0300

    Add files via upload

---
+++ tesc#.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Programa 
+{
+    static void Main()
+    {
+        Console.WriteLine("Olá, mundo!");
+        Console.WriteLine("tecle enter de fechar ....");
+        Console.ReadLine();
+    }
+}
